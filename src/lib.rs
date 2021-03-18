63ae22a9ea01c4ca4a930298229fe2a7057f6d21
amy null <null@amy.gg>
2021-03-18 19:14:11 -0400

    Unstable grab FnMut

--- src/lib.rs
+++ src/lib.rs
@@ -311,7 +311,7 @@ pub use crate::windows::grab as _grab;
 #[cfg(any(feature = "unstable_grab"))]
 pub fn grab<T>(callback: T) -> Result<(), GrabError>
 where
-    T: Fn(Event) -> Option<Event> + 'static,
+    T: FnMut(Event) -> Option<Event> + 'static,
 {
     _grab(callback)
 }
